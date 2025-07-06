14cf121e7db4b20d98f98a18576b72383eae852d
aster <1105950073@qq.com>
2025-07-07 02:51:02 +0800

    生产无法登陆成功,新增环境变量

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,5 @@ import { handlers } from "@/auth"
 // 添加错误处理和日志记录
 export const { GET, POST } = handlers
 
-// 确保 Cloudflare 环境中能够正确处理错误
-export const runtime = 'edge' 
+// // 确保 Cloudflare 环境中能够正确处理错误
+// export const runtime = 'edge'
